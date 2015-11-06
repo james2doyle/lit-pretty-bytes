175d4c5fd9d1bf66421e28d49c77edf8b49a0e7f
James Doyle <james2doyle@gmail.com>
2015-11-06 14:59:54 -0800

    fix wrong description

--- package.lua
+++ package.lua
@@ -2,7 +2,7 @@ return {
   name = "james2doyle/pretty-bytes",
   version = "0.0.3",
   homepage = "https://github.com/james2doyle/lit-pretty-bytes",
-  description = "Get a list of valid text file extensions",
+  description = "Convert bytes to a human readable string",
   tags = {
     "pretty",
     "bytes",
